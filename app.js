035c10947ec40ca42384582ee16dfe0d8628be11
Waldo Lavaut <w86compositor@gmail.com>
2018-05-14 22:53:51 -0400

    Added the tweet button and cleaned up a bit

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 $(document).ready(() => {
-    console.log('ready!');
-
-    let endpoint = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
-
-    let getRandomQuote = () => {
-        jQuery.getJSON(`${endpoint}`, (data, status) => {
-            console.log(status);
-            $("#quote").html(`<i class="fas fa-quote-left"></i> ${data.quoteText} <i class="fas fa-quote-right"></i>`);
-
-            //Checking if the quote does not have an author
-            if (data.quoteAuthor !== "") {
-                $("#quote-author").html(`${data.quoteAuthor} at <a href="${data.quoteLink}" target="_blank" class="card-link">forismatic</a>`);
-            } else {
-                $("#quote-author").html(`Reference at: <a href="${data.quoteLink}" target="_blank" class="card-link">forismatic</a>`);
-            }
-
-        }).fail((error) => {
-            $("#quote").html(`Ah snap...there's a technical error: ${error}`);
-            $("#quote-author").html("");
-        })
-    };
-
-    $("#quote-button").on("click", () => {
-        getRandomQuote();
-    });
-})
+	console.log('ready!');
+	$("i#tweet-icon").on("click", () => {
+		let quote = $.trim($("#quote").text());
+		window.open(`https://twitter.com/intent/tweet?text=${quote}`);
+	});
+	let endpoint = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+	let getRandomQuote = () => {
+		jQuery.getJSON(`${endpoint}`, (data) => {
+			$("#quote").html(`<i class="fas fa-quote-left"></i> ${data.quoteText} <i class="fas fa-quote-right"></i>`);
+			//Checking if the quote does not have an author
+			if (data.quoteAuthor !== "") {
+				$("#quote-author").html(`${data.quoteAuthor} at <a href="${data.quoteLink}" target="_blank" class="card-link">forismatic</a>`);
+			} else {
+				$("#quote-author").html(`Unknown at: <a href="${data.quoteLink}" target="_blank" class="card-link">forismatic</a>`);
+			}
+			//Create a tweet function
+			function tweetQuote() {
+				 window.open(`https://twitter.com/intent/tweet?text=${data.quoteText}`);
+			}
+			$("#tweet-icon").on("click", tweetQuote);
+		}).fail((error) => {
+			$("#quote").text(`Ah snap...there's a technical error: ${error}`);
+			$("#quote-author").text("");
+		});
+	};//end of getRandomQuote
+	$("#quote-button").on("click", getRandomQuote);
+});// end of document.ready
