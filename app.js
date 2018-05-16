f23adc79950e1aa49b90ca66befaf1efc44a8ed2
Waldo Lavaut <w86compositor@gmail.com>
2018-05-16 15:05:52 -0400

    working on the article

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 $(document).ready(() => {
 	console.log('ready!');
+
 	$("i#tweet-icon").on("click", () => {
 		let quote = $.trim($("#quote").text());
 		window.open(`https://twitter.com/intent/tweet?text=${quote}`);
 	});
+
 	let endpoint = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+
 	let getRandomQuote = () => {
-		jQuery.getJSON(`${endpoint}`, (data) => {
+		$.getJSON(`${endpoint}`, (data) => {
 			$("#quote").html(`<i class="fas fa-quote-left"></i> ${data.quoteText} <i class="fas fa-quote-right"></i>`);
+
 			//Checking if the quote does not have an author
 			if (data.quoteAuthor !== "") {
 				$("#quote-author").html(`${data.quoteAuthor} at <a href="${data.quoteLink}" target="_blank" class="card-link">forismatic</a>`);
 			} else {
 				$("#quote-author").html(`Unknown at: <a href="${data.quoteLink}" target="_blank" class="card-link">forismatic</a>`);
 			}
-			//Create a tweet function
-			function tweetQuote() {
-				window.open(`https://twitter.com/intent/tweet?text=${data.quoteText}`);
-			}
-			$("#tweet-icon").on("click", tweetQuote);
-		}).fail((error) => {
-			$("#quote").text(`Ah snap...there's a technical error: ${error}`);
-			$("#quote-author").text("");
 		});
+	
+		//Create a tweet function
+		function tweetQuote() {
+			window.open(`https://twitter.com/intent/tweet?text=${data.quoteText}`);
+		}
+		$("#tweet-icon").on("click", tweetQuote);
+
 	};//end of getRandomQuote
+
 	$("#quote-button").on("click", getRandomQuote);
+
 });// end of document.ready
