d79a3cf1df53e3cd6570ce53a4ce23ff37d7d963
Waldo Lavaut <w86compositor@gmail.com>
2018-05-16 15:06:58 -0400

    folder for another workaround added

---
+++ example/b.js
@@ -0,0 +1,8 @@
+let quotesArray = ["quote1", "quote2", "quote3", "quote4", "quote5", "quote6", "quote7", "quote8"];
+
+
+let randNum = Math.random();
+console.log(randNum);
+
+
+let randQuote = Math.floor(randNum * 
